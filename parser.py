00663660c8e912ebe85e68f9eab0bddb701d0471
bzbzkang <65646883+bzbzkang@users.noreply.github.com>
2022-06-27 21:59:04 +0900

    start again from beginning

--- parser.py
+++ parser.py
@@ -1,26 +1,30 @@
-# from enum import Enum
-
-
-# class MulChoiceToken(Enum):
-#     TITLE = 0
-#     DISTRACTION = 1
-#     KEY = 2
-
-from nltk import CFG, Production, nonterminals
-grammar = CFG.fromstring(
-    """
+# from nltk import CFG, Production, nonterminals
+# grammar = CFG.fromstring(
+#     """
         
-      S -> STEM OPTIONS
-      OPTIONS -> OPTION | 
-      OPTION -> KEY | DISTRACTOR
-      KEY -> KEY_SIGN 
-      DISTRACTOR -> 
-      SEMICOLONE -> ';'
-      KEY_SIGN -> '[*]'
-      DISTRACTOR_SIGN -> '[]'
+#       S -> STEM OPTIONS
+#       OPTIONS -> OPTION | 
+#       OPTION -> KEY | DISTRACTOR
+#       KEY -> KEY_SIGN 
+#       DISTRACTOR -> 
+#       SEMICOLONE -> ';'
+#       KEY_SIGN -> '[*]'
+#       DISTRACTOR_SIGN -> '[]'
       
-      HASH -> '#'
-    """
-)
+#       HASH -> '#'
+#     """
+# )
 
-print(repr(grammar))
+from enum import Enum
+
+class Token(Enum):
+    STEM = 0
+    OPTIONS = 1
+    OPTION = 2
+    KEY = 3
+    KEY_SIGN = 4
+    CONTENT = 5
+    DISTRACTOR = 6
+    DISTRACTOR_SIGN = 7
+    HASH = 8
+    
